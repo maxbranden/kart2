bef2f37f4d40c49203209033a8a2fb6045ff06ad
maxbranden <maxbranden2002@yahoo.no>
2026-08-06 13:31:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -626,7 +626,7 @@ console.log("Kart lastet.");
 console.log("Antall markører:",markerList.length);
 console.log("Script ferdig.");
 
-console.log("VERSJON 23 JULI");
+console.log("VERSJON 08.06. SPIDERY");
 
 // ===============================
 // Update Marker Counts
